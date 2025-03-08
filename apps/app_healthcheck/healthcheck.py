4e16b64701c06acef33b2ee3e54a2a34aef8f1d3
Igor Souza <isouza@g.globo>
2025-03-08 17:14:03 -0300

    Estruturação dos diretórios e criação das apps

---
+++ apps/app_healthcheck/healthcheck.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/healthcheck', methods=['GET'])
+def health_check():
+    # Retorno da mensagem de healthcheck 
+    return "Servidor OK!"
+
+if __name__ == '__main__':
+    # Configuração para  '0.0.0.0', para permitir acessos de qualquer endereço IP
+    app.run(host='0.0.0.0', port=5000)
---
+++ apps/app_mostrador_horas/index.js
@@ -0,0 +1,16 @@
+// Importa o módulo express
+const express = require('express');
+const app = express();
+const port = 5000;
+
+// Rota para retornar a hora atual do servidor
+app.get('/hora', (req, res) => {
+    const dataAtual = new Date();
+    const horaAtual = dataAtual.toLocaleTimeString();
+    res.send(`Hora atual do servidor: ${horaAtual}`);
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
