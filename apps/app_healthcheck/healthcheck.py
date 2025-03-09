84edc883f6eb6472a313a7a31e9ab82ab58dd8a2
Ubuntu <ubuntu@ip-172-31-9-241.ec2.internal>
2025-03-09 17:53:09 +0000

    Ajuste na mensagem de healthcheck usando CI/CD

--- apps/app_healthcheck/healthcheck.py
+++ apps/app_healthcheck/healthcheck.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/healthcheck', methods=['GET'])
 def health_check():
     # Retorno da mensagem de healthcheck 
-    return "Servidor OK!"
+    return "Servidor OK."
 
 if __name__ == '__main__':
     # Configuração para  '0.0.0.0', para permitir acessos de qualquer endereço IP
